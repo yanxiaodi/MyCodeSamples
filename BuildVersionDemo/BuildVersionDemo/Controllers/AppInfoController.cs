73f8c3033959e6ce4624e538567360e3cff4ba56
xiaodi.yan <yan_xiaodi@hotmail.com>
2022-01-23 21:17:46 +1300

    Add BuildVersionDemo

---
+++ BuildVersionDemo/BuildVersionDemo/Controllers/AppInfoController.cs
@@ -0,0 +1,20 @@
+﻿using BuildVersionDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace BuildVersionDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppInfoController : ControllerBase
+    {
+        [HttpGet(Name = "GetAppInfo")]
+        public AppInfo Get()
+        {
+            return new AppInfo()
+            {
+                Version = Assembly.GetEntryAssembly()?.GetName().Version?.ToString() ?? "1.0.0"
+            };
+        }
+    }
+}
---
+++ BuildVersionDemo/BuildVersionDemo/Models/AppInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace BuildVersionDemo.Models
+{
+    public class AppInfo
+    {
+        public string Name { get; set; } = "BuildVersionDemo";
+        public string Version { get; set; } = string.Empty;
+    }
+}
